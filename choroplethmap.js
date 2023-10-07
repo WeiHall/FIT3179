e93f97120c75b26acc2b0856d90d7cb18d4912d9
WeiHall <142386104+WeiHall@users.noreply.github.com>
2023-10-07 15:07:26 +1100

    Delete choroplethmap.js

--- choroplethmap.js
+++
@@ -1,5 +0,0 @@
-var choropleth = "homework2.vg.json"
-
-vegaEmbed("#choro_map", choropleth).then(function(result)   {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
