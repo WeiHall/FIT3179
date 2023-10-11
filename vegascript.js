eb6bf498a261d6daf4e5510c9a7e825d30c0dcc3
WeiHall <142386104+WeiHall@users.noreply.github.com>
2023-10-11 14:08:16 +1100

    Add files via upload

--- vegascript.js
+++ vegascript.js
@@ -1,10 +1,40 @@
 var choropleth = "homework2.vg.json"
 var scatter = "scatterplot.vg.json"
+var boxplot = "boxplot.vg.json"
+var position = "positions.vg.json"
+var scatterdef = "scatterplotdef.vg.json"
+var mancity = "mancity.vg.json"
+var astonvillagoals = "astonvillagoals.vg.json"
+var infringe = "infringements.vg.json"
 
 vegaEmbed("#choro_map", choropleth).then(function(result)   {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 vegaEmbed("#scatterp", scatter).then(function(result)   {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#box", boxplot).then(function(result)   {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#position", position).then(function(result)   {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#scatterdef", scatterdef).then(function(result)   {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#mancity", mancity).then(function(result)   {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#astonvillagoals", astonvillagoals).then(function(result)   {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#infringements", infringe).then(function(result)   {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
